baf3df409867f7d58c03785d57b0f10a05a8cb54
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-15 18:27:36 -0400

    Vehicle classcreat

---
+++ Group8_F2019_MAD3004/Vehicle.swift
@@ -0,0 +1,9 @@
+//
+//  Vehicle.swift
+//  Group8_F2019_MAD3004
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
---
+++ Group8_F2019_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Group8_F2019_MAD3004
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
