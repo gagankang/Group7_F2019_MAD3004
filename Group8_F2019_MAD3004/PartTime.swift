019911b7beb093e24330448224302d29411539e2
shiji703 <shijieugin3@gmail.com>
2019-10-21 20:41:58 -0400

    create class PartTime

---
+++ Group8_F2019_MAD3004/PartTime.swift
@@ -0,0 +1,14 @@
+//
+//  PartTime.swift
+//  Group8_F2019_MAD3004
+//
+//  Created by MacStudent on 2019-10-21.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class PartTime
+{
+    
+}
