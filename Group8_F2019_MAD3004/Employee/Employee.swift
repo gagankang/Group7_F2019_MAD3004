23eee91711ee7ea5512d585d6a9b169ae01976cf
Rajdeep Kaur <chahal.rajdeep03@gmail.com>
2019-10-22 18:50:51 -0400

    changes in employee class

--- Group8_F2019_MAD3004/Employee/Employee.swift
+++ Group8_F2019_MAD3004/Employee/Employee.swift
@@ -13,34 +13,28 @@ class Employee: IPrintable
     var empName: String
     var empAge: Int
     var earning: Int
-    var vehicle: Vehicle?
+    var eType: String
     var calcBirthYear: Int
     {
-        return 2018-self.empAge
+        return 2019-self.empAge
     }
-    init(empID: Int, empName: String, empAge: Int, vehicle: Vehicle?, earning: Int)
+    init(empID: Int, empName: String, empAge: Int, earning: Int, eType: String)
     {
         self.empID = empID
         self.empName = empName
         self.empAge = empAge
         self.earning = earning
-        self.vehicle = vehicle
+        self.eType = eType
     }
     func calcEarnings()
     {
-        self.earning = 0
+    
     }
     func printData()
     {
         print("Employee ID: \(self.empID)")
         print("Employee Name: \(self.empName)")
         print("Employee Birth Year: \(self.calcBirthYear)")
-        if let veh = vehicle
-        {
-            veh.printData()
-        }
-        else{
-            print("This Employee has No vehicle")
-        }
+        
     }
 }
