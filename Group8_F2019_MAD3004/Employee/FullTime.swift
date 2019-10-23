644823c45e9ed92e75d47be22c2b72f9227cbd52
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-23 19:58:57 -0400

    return

--- Group8_F2019_MAD3004/Employee/FullTime.swift
+++ Group8_F2019_MAD3004/Employee/FullTime.swift
@@ -10,8 +10,8 @@
 import Foundation
 class FullTime: Employee
 {
-    var salary: Float?
-    var bonus: Float?
+    var salary: Float
+    var bonus: Float
     
     init(salary: Float, bonus: Float ,empID: String, empName: String, empAge: Int, earning: Float, empType: eType)
     {
@@ -20,13 +20,19 @@ class FullTime: Employee
         super.init(empID: empID, empName: empName, empAge: empAge, earning: earning, empType: empType)
         
     }
+    override func calcEarnings() -> Float
+    {
+        var empearning = salary + bonus
+        return empearning
+        
+    }
     
     override func printData()
     {
         print("Employee is Full time")
         super.printData()
-        print("Salary: \(salary?.formatSalary())")
-        print("Bonus: \(bonus?.formatSalary())")
+        print("Salary: \(salary.formatSalary())")
+        print("Bonus: \(bonus.formatSalary())")
     }
     
     
